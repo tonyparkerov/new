46da47ff1aa980f585ca0e22c8f098602fb34f54
tony <antonproskurov@gmail.com>
2021-01-13 20:32:08 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Hi!");
+let numberOfFilms = prompt('How much filma have u already seen?', 0);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastSeenFilm = prompt ('Last seen film?', 'film name');
+let lastSeenFilmRate = prompt ('His Rate?', 'rate');
+
+personalMovieDB.movies[lastSeenFilm] = lastSeenFilmRate;
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.movies);
